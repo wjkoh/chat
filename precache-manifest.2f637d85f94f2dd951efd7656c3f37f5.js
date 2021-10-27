d305f7adefb666f860c784312ffccde9c76de274
Woojong Koh <wjngkoh@gmail.com>
2021-10-27 16:14:57 +0900

    Deploy composed-react-app

---
+++ precache-manifest.2f637d85f94f2dd951efd7656c3f37f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e89446da2e6d1bc3052f24eeeab51b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b386156784c2db531795",
+    "url": "./static/css/2.9c85336b.chunk.css"
+  },
+  {
+    "revision": "997de2177818eae2feaa",
+    "url": "./static/css/main.4ee8c2d0.chunk.css"
+  },
+  {
+    "revision": "b386156784c2db531795",
+    "url": "./static/js/2.4e5b09a0.chunk.js"
+  },
+  {
+    "revision": "3f0e7270d9361d86cdedbbc2d6ca185f",
+    "url": "./static/js/2.4e5b09a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997de2177818eae2feaa",
+    "url": "./static/js/main.4daa343d.chunk.js"
+  },
+  {
+    "revision": "b81c9e6fd0206bfb5932",
+    "url": "./static/js/runtime-main.859aa5a7.js"
+  }
+]);
